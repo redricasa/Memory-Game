cd1017a8e29fc86d28a0eb1c10d08c064987c690
redricasa <fredeb26@gmail.com>
2019-12-18 17:32:49 -0800

    Updates

---
+++ precache-manifest.bdfa507df8ecb5c38615e3b202827307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9425c4d127405454420cbdad0236f454",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "f9307286dc4a07f4bc5a",
+    "url": "/Memory-Game/static/css/main.13ceccfc.chunk.css"
+  },
+  {
+    "revision": "8829761c7c6f18c6ba67",
+    "url": "/Memory-Game/static/js/2.7a58ef81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Memory-Game/static/js/2.7a58ef81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9307286dc4a07f4bc5a",
+    "url": "/Memory-Game/static/js/main.a5ff8810.chunk.js"
+  },
+  {
+    "revision": "fc560fc682f2cef967e3",
+    "url": "/Memory-Game/static/js/runtime-main.37751621.js"
+  }
+]);
